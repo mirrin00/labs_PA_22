7d3e4fb8cffaa812418bb25fb62fb796891c3909
mirrin00 <mirrin1905@outlook.com>
2022-09-29 21:04:44 +0100

    Added report

--- 9304_Tinyakov_Sergey_lb1/run_evaluation.sh
+++ 9304_Tinyakov_Sergey_lb1/run_evaluation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+make
+
 max_threads=100
 sizes="10 100 1000 10000"
 matrix1="matrix1.bin"
@@ -11,7 +13,7 @@ do
     echo "Generating matrices with size $size x $size"
     ./generate_matrix $size $size $matrix1 bin
     ./generate_matrix $size $size $matrix2 bin
-    for ((i = 0; i <= $max_threads; i++))
+    for ((i = 1; i <= $max_threads; i++))
     do
         echo "Runnig with $i threads..."
         ./thread_sum $matrix1 $matrix2 $res_matrix $i bin >> result.log
