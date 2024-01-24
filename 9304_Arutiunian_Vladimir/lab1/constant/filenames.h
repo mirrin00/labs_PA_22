2ee2774846e8a6bb2e719f05d0059ccc56068ed6
mirrin00 <mirrin1905@outlook.com>
2024-01-24 22:43:43 +0300

    Delete groupmates' work

--- 9304_Arutiunian_Vladimir/lab1/constant/filenames.h
+++
@@ -1,9 +0,0 @@
-#ifndef LABS_PA_22_FILENAMES_H
-#define LABS_PA_22_FILENAMES_H
-
-namespace filenames {
-    const auto kMatrixInputFilename = "matrices.txt";
-    const auto kMatrixOutputFilename = "out_matrix.txt";
-}
-
-#endif //LABS_PA_22_FILENAMES_H
